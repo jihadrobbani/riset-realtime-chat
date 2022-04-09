2935306baccb576c40341aa09547b61ada6b3bfd
jihadrobbani <70126615+jihadrobbani@users.noreply.github.com>
2022-04-09 22:06:46 +0700

    Update server

--- server/config/database.config.ts
+++ server/config/database.config.ts
@@ -1,19 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
+const db = new Sequelize(process.env.DATABASE_URL!, {
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
     },
-    logging: false,
-  }
-);
+  },
+  logging: false,
+});
 
 export default db;
